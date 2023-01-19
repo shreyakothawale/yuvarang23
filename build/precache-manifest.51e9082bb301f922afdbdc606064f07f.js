b714e6e355315041137e7465c17572365b7330d7
ThunderSmoker <pitlawararadhya@gmail.com>
2023-01-20 01:59:03 +0530

    updated carousel

---
+++ build/precache-manifest.51e9082bb301f922afdbdc606064f07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "166151c42fbb7807bd5c",
+    "url": "/yuvarang23/static/css/main.b1fc9a70.chunk.css"
+  },
+  {
+    "revision": "166151c42fbb7807bd5c",
+    "url": "/yuvarang23/static/js/main.166151c4.chunk.js"
+  },
+  {
+    "revision": "493d9ad790483bb94450",
+    "url": "/yuvarang23/static/css/1.18838186.chunk.css"
+  },
+  {
+    "revision": "493d9ad790483bb94450",
+    "url": "/yuvarang23/static/js/1.493d9ad7.chunk.js"
+  },
+  {
+    "revision": "9c61ff915b45adce483a",
+    "url": "/yuvarang23/static/js/runtime~main.9c61ff91.js"
+  },
+  {
+    "revision": "58a9466bb19849054d1b0398c3eee6bf",
+    "url": "/yuvarang23/index.html"
+  }
+];
